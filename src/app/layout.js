8568e0af3db4cd97bc18d3eba71c24bc0e805941
Vishnu <110332107+vishnu7198@users.noreply.github.com>
2023-06-05 13:09:50 +0530

    Doctor

---
+++ src/app/layout.js
@@ -0,0 +1,15 @@
+import './globals.css'
+import Navbar from './navbar'
+
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <>
+    <h2>home page...</h2>
+    </>
+  )
+}
